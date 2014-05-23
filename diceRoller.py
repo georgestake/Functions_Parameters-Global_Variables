14a61036625f7d2ecb07c8ff03a9a3a0314253a7
georgestake <georgerobson1207@gmail.com>
2014-05-23 08:52:44 +0100

    Update diceRoller.py

--- diceRoller.py
+++ diceRoller.py
@@ -1,8 +1,8 @@
-#Coin flip program
+die roll program
 #Describe the purpose of this program here.
 
-inport random;time
-
+import random,time
+#dice face
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
 s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
@@ -11,24 +11,30 @@ s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
 
 def roll():
-    Print("rolling....."
-    roll = rand.randing(7)
-
-
-def show_dice(roll)
-    if roll = 1:
-        print(S1)
-    else roll = 2
+    roll = random.randint(1,6)#picks a random number from 1-6
+    return roll
+#displays the andom number in its dice form
+def show_dice(roll):
+    if roll == 1:
+        print(s1)
+    elif roll == 2:
         print(s2)
-    else roll = 3
+    elif roll == 3:
         print(s3)
-    else roll = 4
+    elif roll ==4:
         print(s4)
-    else roll = 5
+    elif roll == 5:
         print(s5)
-    else roll = 6
+    elif roll == 6:
         print(s6)
 
-roll
-time.sleep(1)
-show_dice(roll)
+def roll_dice():
+    myroll = roll()
+    time.sleep(1)
+    show_dice(myroll)
+def debug():#funtion for debuging
+    while True:#for debugging purposes it infinetely repeats diferent random numbers so i could check if all numbers would be displayed
+        myroll = roll()
+        time.sleep(1)
+        show_dice(myroll)#displays myroll
+      
